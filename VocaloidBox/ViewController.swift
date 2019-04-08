7b24b1bcc0aa5185fabd464ebe6bdb11644d4c64
CorentinDupont <dupont.corentin44@gmail.com>
2019-04-08 09:00:35 +0200

    Initial Commit

---
+++ VocaloidBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VocaloidBox
+//
+//  Created by Corentin Dupont on 08/04/2019.
+//  Copyright © 2019 Corentin Dupont. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
